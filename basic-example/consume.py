5653f4e25474be9d1b01da2c9891232475bbaec6
mark kelly <mkelly28@tcd.ie>
2018-10-10 09:49:21 +0100

    Docker compose fix with python test (#57)

--- basic-example/consume.py
+++ basic-example/consume.py
@@ -5,17 +5,16 @@ import logging as log
 log.basicConfig(level=log.INFO)
 
 
-def consume(varargs):
-    ip = varargs[0]
-    print(ip + ":9092")
+def consume():
     consumer = KafkaConsumer(
         group_id='test-0-consumer',
-        bootstrap_servers=ip + ":9092")
+        bootstrap_servers="localhost:9092")
     consumer.subscribe(['test-0'])
+    consumer.subscription()
 
     for message in consumer:
         print (message)
 
 
 if __name__ == "__main__":
-    consume(sys.argv[1:])
+    consume()
--- basic-example/produce.py
+++ basic-example/produce.py
@@ -3,9 +3,10 @@ from kafka import KafkaProducer
 
 
 def produce(varargs):
-    ip = varargs[0]
-    producer = KafkaProducer(bootstrap_servers=ip + ":9092")
-    for _ in range(10):
+    numberrange = int(varargs[0])
+    print numberrange
+    producer = KafkaProducer(bootstrap_servers="localhost:9092")
+    for _ in range(numberrange):
         print("Producing message to topic test-0")
         producer.send('test-0', b'some_message_bytes')
 
