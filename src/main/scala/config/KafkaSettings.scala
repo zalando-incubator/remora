745691d2e869b4e7da693f528328590d0e40fbab
Jordan Moore <crikket.007@gmail.com>
2019-01-04 14:51:46 -0600

    Better Healtcheck (#46)

--- src/main/scala/config/KafkaSettings.scala
+++ src/main/scala/config/KafkaSettings.scala
@@ -1,8 +1,16 @@
 package config
 
+import scala.collection.Map
+import scala.collection.JavaConverters._
 import com.typesafe.config.Config
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.admin.AdminClient
 
-case class KafkaSettings(address: String, commandConfig: String)
+case class KafkaSettings(address: String, commandConfig: String) {
+  lazy val adminClient: AdminClient = AdminClient.create(Map[String, AnyRef](
+    CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG -> address
+  ).asJava)
+}
 
 object KafkaSettings {
   def apply(config: Config): KafkaSettings =
